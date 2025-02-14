aa63015fefe6d9292e9501dcc58b2d6d5fabe3ae
adriano <72319679+banana-bread@users.noreply.github.com>
2025-02-13 19:31:21 -0500

    remove tf ssm (#6)

--- main.tf
+++ main.tf
@@ -35,36 +35,9 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "falcons-stats-ssm-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = { Service = "ec2.amazonaws.com" },
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "ssm_access" {
-  name       = "ssm-access"
-  roles      = [aws_iam_role.ec2_ssm_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "falcons-stats-ssm-profile"
-  role = aws_iam_role.ec2_ssm_role.name
-}
-
 resource "aws_instance" "falcons_stats_server" {
   ami                    = "ami-085ad6ae776d8f09c"
   instance_type          = "t2.micro"
-  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
   vpc_security_group_ids = [aws_security_group.instances.id]
 
   tags = {
